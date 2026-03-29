7d0af86d200f23eb56aa6ff69df19ed262bc7a15
Zhiqiang Liao <zhiqiang.liao@aalto.fi>
2026-03-29 16:35:10 +0800

    update test

--- test.py
+++ test.py
@@ -1,38 +1,31 @@
 import numpy as np
 from Models.AHP import AHP_normalization
 
-preferences = np.array([[1, 1/5, 1/3, 1/2],
-                        [5, 1, 2, 4],
-                        [3, 1/2, 1, 3],
-                        [2, 1/4, 1/3, 1]])
+C = np.array([[1, 3, 5],
+              [1/3, 1, 3],
+              [1/5, 1/3, 1]])
 
-evaluation1 = np.array([[1, 1/2, 1/3, 5],
-                       [2, 1, 1/2, 7],
-                       [3, 2, 1, 9],
-                       [1/5, 1/7, 1/9, 1]])
-evaluation2 = np.array([[1, 1/2, 3, 5],
-                       [2, 1, 1/2, 7],
-                       [1/3, 2, 1, 9],
-                       [1/5, 1/7, 1/9, 1]])
-evaluation3 = np.array([[1, 1/2, 1/3, 1/5],
-                       [2, 1, 1/2, 7],
-                       [3, 2, 1, 9],
-                       [5, 1/7, 1/9, 1]])
-evaluation4 = np.array([[1, 1/2, 1/3, 5],
-                       [2, 1, 1/2, 7],
-                       [3, 2, 1, 1/9],
-                       [1/5, 1/7, 9, 1]])
+B1 = np.array([[1, 3, 7],
+              [1/3, 1, 5],
+              [1/7, 1/5, 1]])
 
-scores = np.array([[0.174, 0.293, 0.489, 0.044],
-                  [0.050, 0.444, 0.312, 0.194],
-                  [0.210, 0.038, 0.354, 0.398],
-                  [0.510, 0.012, 0.290, 0.188]])
+B2 = np.array([[1, 1/5, 1],
+              [5, 1, 5],
+              [1, 1/5, 1]])
 
-ahp = AHP_normalization(preferences, [evaluation1, evaluation2, evaluation3, evaluation4])
+B3 = np.array([[1, 5, 9],
+              [1/5, 1, 3],
+              [1/9, 1/3, 1]])
+B = [B1, B2, B3]
+
+ahp = AHP_normalization(C, B)
+
 weigts = ahp.weight_vector()
 scores = ahp.score_matrix()
 global_score = ahp.global_score()
 CI = ahp.CI()
-# print("Weights:", weigts)
-# print("Scores:", scores)
+
+print("Weights:", weigts)
+print("Scores:", scores)
+print("Global Score:", global_score)
 print("CI:", CI)
